da5e0d0cdaf052b993863683f2359f76f9cf4df6
Gonçalo Dessin das Graças <ggomes814@gmail.com>
2026-03-30 05:12:28 +0000

    chore: remove build/ from tracking

--- build/sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/openobsw/sim
-# Build directory: /workspaces/openobsw/build/sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/test/CMakeFiles/test_space_packet.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_space_packet.
-# This may be replaced when dependencies are built.
