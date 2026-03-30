ea73806e6b3521406b340047f3912581b05c667f
Gonçalo Dessin das Graças <ggomes814@gmail.com>
2026-03-30 05:12:28 +0000

    chore: remove build/ from tracking

--- build/_deps/unity-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/openobsw/build/_deps/unity-src
-# Build directory: /workspaces/openobsw/build/_deps/unity-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/src/CMakeFiles/obsw.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for obsw.
-# This may be replaced when dependencies are built.
