7038974eb91441edcb336580a3c80c4ef18d0472
Gonçalo Dessin das Graças <ggomes814@gmail.com>
2026-04-01 06:13:15 +0000

    feat: S6 memory mgmt, S8 function mgmt, fsm_to_nominal, recover via S8

--- include/obsw/obsw.h
+++ include/obsw/obsw.h
@@ -8,19 +8,21 @@
 #include "obsw/ccsds/space_packet.h"
 #include "obsw/ccsds/tc_frame.h"
 #include "obsw/ccsds/tm_frame.h"
-#include "obsw/tc/dispatcher.h"
-#include "obsw/tm/store.h"
+#include "obsw/fdir/fsm.h"
+#include "obsw/fdir/watchdog.h"
 #include "obsw/hal/io.h"
 #include "obsw/pus/pus_tm.h"
 #include "obsw/pus/s1.h"
+#include "obsw/pus/s17.h"
 #include "obsw/pus/s3.h"
 #include "obsw/pus/s5.h"
-#include "obsw/pus/s17.h"
-#include "obsw/fdir/fsm.h"
-#include "obsw/fdir/watchdog.h"
+#include "obsw/pus/s6.h"
+#include "obsw/pus/s8.h"
+#include "obsw/tc/dispatcher.h"
+#include "obsw/tm/store.h"
 
 #define OBSW_VERSION_MAJOR 0
-#define OBSW_VERSION_MINOR 4
+#define OBSW_VERSION_MINOR 5
 #define OBSW_VERSION_PATCH 0
 
 #endif /* OBSW_H */
